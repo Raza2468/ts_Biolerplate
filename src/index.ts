68aa45a37139ac55f2f636f599743f4c92e3c78a
Raza <71090111+Raza2468@users.noreply.github.com>
2022-12-16 00:43:34 +0500

    typscript_Biolerplate

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+import chalk from 'chalk';
+
+
+let typeScript_Biolerplate = await inquirer.prompt([{
+    name: "Biolerplate",
+    type: String,
+    message: "welcom to the type Script Biolerplate"
+}]);
+
+console.log(chalk.green(typeScript_Biolerplate.Biolerplate));
